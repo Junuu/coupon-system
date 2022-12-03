2164d0fcd9a01694343f723e63c6e6e0f0ddfbc7
junuu <bababrll@naver.com>
2022-12-04 01:58:14 +0900

    feat: make single module to multi module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,51 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.6"
-	id("io.spring.dependency-management") version "1.1.0"
+	id("org.springframework.boot") version "2.7.6" apply false
+	id("io.spring.dependency-management") version "1.1.0" apply false
 	kotlin("jvm") version "1.7.21"
-	kotlin("plugin.spring") version "1.7.21"
+	kotlin("plugin.spring") version "1.7.21" apply false
 }
 
-group = "com.demo"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+allprojects{
+	group = "com.demo"
+	version = "0.0.1-SNAPSHOT"
 
-repositories {
-	mavenCentral()
-}
+	tasks.withType<JavaCompile>{
+		sourceCompatibility = "11"
+		targetCompatibility = "11"
+	}
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "11"
+		}
+	}
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
+	repositories {
+		mavenCentral()
 	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+subprojects{
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+
+	dependencies {
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+	}
 }
+
+
+
+
