9116fa9c75d678df6a61888e4eae6d27537cd138
junuu <bababrll@naver.com>
2022-12-04 01:36:05 +0900

    feat: init project setting

---
+++ src/main/kotlin/com/demo/demo/CouponApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponApplication
+
+fun main(args: Array<String>) {
+	runApplication<CouponApplication>(*args)
+}
